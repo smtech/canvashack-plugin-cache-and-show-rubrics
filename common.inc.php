30e98da633c7d13a573d3bb011adefe34b463358
Seth Battis <sethbattis@stmarksschool.org>
2016-10-24 14:23:22 -0400

    Fiddling around...
    ...trying to make the dang thing read right.

--- common.inc.php
+++ common.inc.php
@@ -1,12 +1,14 @@
 <?php
 
-require_once(__DIR__ . '/../common.inc.php');
+require_once __DIR__ . '/../common.inc.php';
+
+use Battis\AppMetadata;
 
 if (file_exists(__DIR__ . '/manifest.xml')) {
-	$manifest = simplexml_load_string(file_get_contents(__DIR__ . '/manifest.xml'));
+    $manifest = simplexml_load_string(file_get_contents(__DIR__ . '/manifest.xml'));
 }
 
-$pluginMetadata = new Battis\AppMetadata($sql, (string) $manifest->id);
+$pluginMetadata = AppMetadata($sql, (string) $manifest->id);
 
 $smarty->addTemplateDir(__DIR__ . '/templates', (string) $manifest->id);
 
