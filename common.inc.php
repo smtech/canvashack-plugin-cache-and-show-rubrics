5b59f38a9212a42ad841ce24378f1473fa1f371a
Seth Battis <sethbattis@stmarksschool.org>
2016-10-24 14:17:50 -0400

    Cleaning up cruft

--- common.inc.php
+++ common.inc.php
@@ -8,13 +8,6 @@ if (file_exists(__DIR__ . '/manifest.xml')) {
 
 $pluginMetadata = new Battis\AppMetadata($sql, (string) $manifest->id);
 
-$pluginMetadata['PLUGIN_PATH'] = __DIR__;
-$pluginMetadata['PLUGIN_URL'] = (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on' ? 'http://' : 'https://') . $_SERVER['SERVER_NAME'] . preg_replace("|^{$_SERVER['DOCUMENT_ROOT']}(.*)$|", '$1', $pluginMetadata['PLUGIN_PATH']);
-$pluginMetadata['PLUGIN_ID'] = (string) $manifest->id;
-$pluginMetadata['PLUGIN_NAMESPACE'] = preg_replace('/[^a-z]+/i', '_', "smtech_{$pluginMetadata['PLUGIN_ID']}");
-$pluginMetadata['RESPONSE_SUCCESS'] = 'success';
-$pluginMetadata['RESPONSE_FAILURE'] = 'failure';
-
-$smarty->addTemplateDir(__DIR__ . '/templates', $pluginMetadata['PLUGIN_ID']);
+$smarty->addTemplateDir(__DIR__ . '/templates', (string) $manifest->id);
 
 ?>
